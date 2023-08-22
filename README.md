cfc9e42fef50bf690cb74d1c8d09cd00e26dad6b
Rotfus <140560063+Rotfus@users.noreply.github.com>
2023-08-22 21:44:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# import:
+I will use import csv, import os and later will use another helper.py file and will import from there.
+
+
+# cars
+
+# car{brand,model,color}
+
+# methods (enum)
+Add - done
+Delete
+Print all - print done, numbering still not implemented.
+Search
+Exit - done
+Update
+
+# extra methods - for later progress
+save - on end
+load - on start
+
+
+
---
+++ app.py
@@ -0,0 +1,49 @@
+#import
+from enum import Enum
+import os
+import csv
+
+#Methods (enum)
+#Add
+#Delete
+#print
+#search
+#exit
+
+class Actions(Enum):
+    PRINT = 0
+    ADD = 1
+    DELETE = 2
+    SEARCH = 3
+    UPDATE = 4
+    EXIT = 6
+
+cars=[]
+
+def save_to_csv():
+    with open("cars.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(cars)
+      
+
+
+def display_menu():
+    for x in Actions: print(f'{x.value} - {x.name}')
+    return Actions(int(input('Your selection')))
+
+
+
+def main():
+    while(True):
+        user_selection=display_menu()
+        if user_selection== Actions.PRINT: print(cars)
+        if user_selection== Actions.ADD: cars.append({"brand":input("Please enter car brand"), "model":input("Please enter car model"), "color":input("Please enter car color")})
+        if user_selection== Actions.EXIT: return 
+
+
+
+
+if __name__=='__main__':
+    main()
+    save_to_csv()
+    print("Thank you for visiting the garage :)")
